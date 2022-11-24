aee80f93d3c05b16fe6a77191bc5fe0f675f8172
Bhupender98 <bhupander17440@gmail.com>
2022-11-24 19:57:07 +0530

    second-commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="Navbar mb-4">
+      <nav
+        className="navbar navbar-light justify-content-between"
+        style={{ backgroundColor: "#00000" }}
+      >
+        <Link to="/" className="navbar-danger m-2 ms-5">
+          {" "}
+          <h2>Habit-Tracker</h2>
+        </Link>
+        <form action="" className="form-inline">
+          <Link
+            to="/add-habit"
+            className="btn btn-outline-success me-5 my-sm-0 m-2"
+            type="submit"
+          >
+            Add Habits
+          </Link>
+        </form>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
